69cdd9949f90b873a176c5642a6453db026d05aa
elmanzeynalov <elman.zeynalov@gmail.com>
2025-02-16 01:49:38 +0400

    PostCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
+import PostCard from '@/components/PostCard';
+
 export default function Home() {
 	return (
 		<main className='grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
-			<h1>home</h1>
-			<h1>home</h1>
-			<h1>home</h1>
-			<h1>home</h1>
-			<h1>home</h1>
-			<h1>home</h1>
-			<h1>home</h1>
-			<h1>home</h1>
+	<PostCard/><PostCard/><PostCard/><PostCard/><PostCard/><PostCard/><PostCard/><PostCard/><PostCard/><PostCard/>
 		</main>
 	);
 }

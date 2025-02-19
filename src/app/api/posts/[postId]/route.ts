ebc7d900dfde535d656e0b7a5873298d0095efa3
elmanzeynalov <elman.zeynalov@gmail.com>
2025-02-19 14:51:51 +0400

    create route delete patch post

---
+++ src/app/api/posts/[postId]/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/db';
+
+interface ContextProps {
+	params: {
+		postId: string;
+	};
+}
+
+export async function DELETE(req: Request, context: ContextProps) {
+	try {
+		const { params } = context;
+		// console.log('isjaskjskjjksa', params);
+		await db.post.delete({
+			where: {
+				id: params.postId,
+			},
+		});
+		// console.log(tags);
+		return new Response(null, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ massage: 'not delete post' }, { status: 500 });
+	}
+}
+
+export async function PATCH(req: Request, context: ContextProps) {
+	try {
+		const { params } = context;
+		// console.log('isjaskjskjjksa', params);
+
+		const body = await req.json();
+		await db.post.update({
+			where: {
+				id: params.postId,
+			},
+			data: {
+				title: body.title,
+				content: body.content,
+				tagId: body.tagId,
+			},
+		});
+		// console.log(tags);
+		return NextResponse.json({ massage: 'update success' }, { status: 200 });
+	} catch (error) {
+		return NextResponse.json({ massage: 'not delete post' }, { status: 500 });
+	}
+}
